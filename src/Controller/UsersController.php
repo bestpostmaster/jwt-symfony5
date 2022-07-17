77c0dfdb0f82b9859eb1c5989c2b7ccaf728489d
Mohamed <bestpostmaster@gmail.com>
2022-07-17 20:10:06 +0200

    install Serializer and return Users

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +12,8 @@ class UsersController extends AbstractController
     /**
      * @Route("/api/admin/users", name="app_users")
      */
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
-        return JsonResponse::fromJsonString('{ "users": ["user1", "user2", "user3"] }');
+        return $this->json($userRepository->findAll(), 200, [], ['groups' => 'user:read']);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
@@ -15,16 +16,19 @@ class User implements UserInterface
      * @ORM\Id
      * @ORM\GeneratedValue
      * @ORM\Column(type="integer")
+     * @groups("user:read")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @groups("user:read")
      */
     private $login;
 
     /**
      * @ORM\Column(type="json")
+     * @groups("user:read")
      */
     private $roles = [];
 
